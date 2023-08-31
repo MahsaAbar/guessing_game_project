3d6a857e6cd71e86e90910d19d30fbc3e3c3d9bb
unknown <abarghouei.mahsa@gmail.com>
2023-08-31 21:21:58 +0330

    completed guessing_game_project

--- fav-num-v2.js
+++ fav-num-v2.js
@@ -21,30 +21,25 @@ const checkGuess = (guess_num, secretNumber) =>{
 rl.question('Enter a max number : ', max_num =>{
     rl.question('Enter a min number : ',min_num =>{
         console.log("I'm thinking of a number between " +"("+  Number(min_num) + "," + Number(max_num)+ ")"+" ...");
-        console.log("You can enter 3 guesses !!!")
+        console.log("You have 3 guesses !!!")
         let min_number = Number(min_num);
         let max_number = Number(max_num);
         let secretNumber =  Math.floor(Math.random() * (max_number - min_number) + min_number);
 
         rl.question ('Enter your first guess:', guess_num =>{
-            console.log("You're number is " + Number(guess_num))
-            console.log('the random number is '+ secretNumber)
             if(checkGuess(Number(guess_num), secretNumber)){
                 console.log("YOU WON")
             }else{
                 rl.question('Enter your second guess:', guess_num =>{
-                    console.log("You're number is " + Number(guess_num))
-                    console.log('the random number is '+ secretNumber)
                     if(checkGuess(Number(guess_num), secretNumber)){
                         console.log("YOU WON")
                     }else{
                         rl.question('Enter your last guess:', guess_num =>{
-                            console.log("You're number is " + Number(guess_num))
-                            console.log('the random number is '+ secretNumber)
                             if(checkGuess(Number(guess_num), secretNumber)){
                                 console.log("YOU WON")
                             }else{
-                                console.log("OooOOoOps ... You Loss")
+                                console.log("OooOOoOps ... You Loss :( ")
+                                console.log('The random number was '+ secretNumber)
                             }
                             rl.close()
                         })
