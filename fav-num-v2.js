b4b462a08c148ec31820a80e0931884a544fc64d
unknown <abarghouei.mahsa@gmail.com>
2023-08-31 21:10:47 +0330

    completed guessing_game_project

--- fav-num-v2.js
+++ fav-num-v2.js
@@ -4,9 +4,8 @@ const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
-const secretNumber = 12
 
-const checkGuess = guess_num =>{
+const checkGuess = (guess_num, secretNumber) =>{
     if(guess_num > secretNumber){
         console.log("Too high.")
         return false
@@ -19,37 +18,39 @@ const checkGuess = guess_num =>{
     }
 };
 
-rl.question('Enter a max number:',(max_num)=>{
+rl.question('Enter a max number : ', max_num =>{
+    rl.question('Enter a min number : ',min_num =>{
+        console.log("I'm thinking of a number between " +"("+  Number(min_num) + "," + Number(max_num)+ ")"+" ...");
+        console.log("You can enter 3 guesses !!!")
+        let min_number = Number(min_num);
+        let max_number = Number(max_num);
+        let secretNumber =  Math.floor(Math.random() * (max_number - min_number) + min_number);
 
-    rl.question('Enter a min number:',(min_num)=>{
-        console.log(`I'm thinking of a number between ${Number(min_num)} and ${Number(max_num)} ...`);
-
-        rl.question('Enter a guess:', handleFirstGuess);
-    });
-     
+        rl.question ('Enter your first guess:', guess_num =>{
+            console.log("You're number is " + Number(guess_num))
+            console.log('the random number is '+ secretNumber)
+            if(checkGuess(Number(guess_num), secretNumber)){
+                console.log("YOU WON")
+            }else{
+                rl.question('Enter your second guess:', guess_num =>{
+                    console.log("You're number is " + Number(guess_num))
+                    console.log('the random number is '+ secretNumber)
+                    if(checkGuess(Number(guess_num), secretNumber)){
+                        console.log("YOU WON")
+                    }else{
+                        rl.question('Enter your last guess:', guess_num =>{
+                            console.log("You're number is " + Number(guess_num))
+                            console.log('the random number is '+ secretNumber)
+                            if(checkGuess(Number(guess_num), secretNumber)){
+                                console.log("YOU WON")
+                            }else{
+                                console.log("OooOOoOps ... You Loss")
+                            }
+                            rl.close()
+                        })
+                    }
+                })
+            }
+        })
+    })
 });
-
-const handleFirstGuess = (guess_num)=>{
-    if(checkGuess(Number(guess_num))){
-        console.log("YOU WON")
-    }else{
-        rl.question('Enter a guess:', handleSecondGuess);
-    }
-};
-
-const handleSecondGuess = (guess_num)=>{
-    if(checkGuess(Number(guess_num))){
-        console.log("YOU WON")
-    }else{
-        rl.question('Enter a guess:',handleThirdGuess);
-    }
-};
-
-const handleThirdGuess = (guess_num)=>{
-    if(checkGuess(Number(guess_num))){
-        console.log("YOU WON")
-    }else{
-        console.log("OooOOoOps ... You Loss")
-    }
-    rl.close()
-};
